baf5b14a5557c1b47d4d9db3500ac35a7cd9fc6e
pegartillo95 <pegarc03@ucm.es>
2016-11-03 15:35:46 +0100

    Minor fix in lambda value

--- ex2_reg.m
+++ ex2_reg.m
@@ -80,8 +80,7 @@ pause;
 % Initialize fitting parameters
 initial_theta = zeros(size(X, 2), 1);
 
-% Set regularization parameter lambda to 1 (you should vary this)
-lambda = 0;
+lambda = 1;
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
